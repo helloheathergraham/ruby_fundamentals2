b0a9509ecb062ba35f61217c2d49c738d955d944
Heather <helloheathergraham@gmail.com>
2014-01-15 12:37:04 -0500

    completed exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -2,7 +2,23 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 grocery_list << "rice"
 
 def groceries(array)
-	array.each {|item| puts "* #{item}"}
+	array.sort.each {|item| puts "* #{item}"}
 end
 
+puts grocery_list.length
+
+if grocery_list.include? "bananas"
+	puts "you need to pick up bananas"
+else
+	puts "You don't need to pick up bananas"
+end
+
+puts grocery_list[1]
+
 groceries(grocery_list)
+
+grocery_list.delete("salmon")
+
+groceries(grocery_list)
+
+
